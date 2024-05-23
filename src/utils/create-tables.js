f351c250b33c4f1be2c7a463f2dea9dd7125240b
talison-cardoso <talisong.me@gmail.com>
2024-05-23 03:59:38 -0300

    feat: add utility to create the tables

--- src/utils/create-tables.js
+++ src/utils/create-tables.js
@@ -2,6 +2,21 @@ import { db } from "../lib/sqlite.js";
 
 export const createTables = async () => {
     db.prepare(
-        "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, username TEXT UNIQUE, password TEXT)"
+        `CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        )`
     ).run();
-}
+    db.prepare(
+        `CREATE TABLE IF NOT EXISTS documents (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            path TEXT NOT NULL,
+            user_id INTEGER NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users(id)
+        )`
+    ).run();
+};
