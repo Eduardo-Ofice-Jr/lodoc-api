4d6e90d7ab249b7e9c0ba56566c05adee97341e1
talison-cardoso <talisong.me@gmail.com>
2024-05-23 04:01:51 -0300

    refactor(user.router): remove routes used in development

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -9,13 +9,4 @@ import {
 router.post("/login", loginController);
 router.post("/register", registerController);
 
-// for development
-import { db } from "../db/config.js";
-
-router.delete("/delete-all", (req, res) => {
-    db.prepare("delete from users").run();
-    db.prepare("drop table if exists users").run();
-    res.status(200).json({ message: "users deleted" });
-});
-
 export default router;
